99a96d1cd86d8953a6bdcbe26bbd5dcd994c487a
shoxiyyyy <34606601+shoxiyyyy@users.noreply.github.com>
2018-03-03 17:50:06 +0100

    Update takikurwasklepowinnienbyc.hpp

--- takikurwasklepowinnienbyc.hpp
+++ takikurwasklepowinnienbyc.hpp
@@ -169,8 +169,12 @@
             { "RH_fnp45t", "", 250, 0, "call life_coplevel >= 1" },
             { "RH_fn57", "", 250, 0, "call life_coplevel >= 1" },
             { "CSW_FN57", "", 250, 0, "call life_coplevel >= 1" },
-            { "CSW_FN57_Ballistic_Shield", "FN57 Z Tarcza", 250, 0, "call life_coplevel >= 1" },
-            
+            { "CSW_FN57_Ballistic_Shield", "FN57 Z Tarcza", 250, 0, "call life_coplevel >= 3" },
+            { "hlc_rifle_bcmjack", "", 250, 0, "call life_coplevel >= 1" },
+            { "hlc_rifle_bcmblackjack", "", 250, 0, "call life_coplevel >= 2" },
+            { "hlc_rifle_RU5562", "", 250, 0, "call life_coplevel >= 1" },
+            { "hlc_rifle_samr2", "", 250, 0, "call life_coplevel >= 2" },
+            { "hlc_rifle_awmagnum_BL", "", 250, 0, "call life_coplevel >= 3" }
         };
         mags[] = {
             { "CSW_Taser_Probe_Mag", "", 250, 0, "call life_coplevel >= 1" },
@@ -185,7 +189,11 @@
             { "RH_15Rnd_45cal_fnp", "", 250, 0, "call life_coplevel >= 1" },
             { "RH_20Rnd_57x28_FN", "", 250, 0, "call life_coplevel >= 1" },
             { "CSW_20Rnd_57x28_SS190", "", 250, 0, "call life_coplevel >= 1" },
-            
+            { "hlc_30rnd_556x45_EPR", "", 250, 0, "call life_coplevel >= 1" },
+            { "29rnd_300BLK_STANAG", "", 250, 0, "call life_coplevel >= 2" },
+            { "hlc_50rnd_300BLK_STANAG_EPR", "", 250, 0, "call life_coplevel >= 3" },
+            { "150Rnd_556x45_Drum_Mag_F", "", 250, 0, "call life_coplevel >= 3" },
+            { "hlc_5rnd_300WM_FMJ_AWMF", "", 250, 0, "call life_coplevel >= 3" }
         };
         accs[] = {
             { "FHQ_optic_AC11704", "", 250, 0, "call life_coplevel >= 1" },
@@ -203,11 +211,14 @@
             { "RH_docter", "", 2500, 0, "call life_coplevel >= 1" },
             { "RH_osprey", "", 250, 0, "call life_coplevel >= 1" },
             { "optic_MRD", "", 250, 0, "call life_coplevel >= 1" },
+            { "acc_flashlight", "", 250, 0, "call life_coplevel >= 1" },
             { "CSW_FN57_flashlight_normal_2", "", 250, 0, "call life_coplevel >= 1" },
             { "CSW_FN57_silencer2", "", 250, 0, "call life_coplevel >= 3" },
             { "CSW_FN57_flashlight_normal_1", "", 250, 0, "call life_coplevel >= 3" },
             { "CSW_FN57_Barska", "", 250, 0, "call life_coplevel >= 3" },
-            { "CSW_FN57_Shield_P", "", 250, 0, "call life_coplevel >= 3" }
+            { "CSW_FN57_Shield_P", "", 250, 0, "call life_coplevel >= 3" },
+            { "hlc_muzzle_300blk_KAC", "", 250, 0, "call life_coplevel >= 2" },
+            { "hlc_muzzle_556NATO_KAC", "", 250, 0, "call life_coplevel >= 2" }
         };
     };
 
